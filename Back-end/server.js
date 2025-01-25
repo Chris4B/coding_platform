c79c0987b778e4388ccb50da2329cb4753f84431
Chris Balla <ballachris13@gmail.com>
2025-01-25 15:01:11 +0100

    feat: integrate yamljs for OpenAPI YAML support in back-end
    - Installed yamljs to parse OpenAPI YAML files.
    - Configured Swagger UI in server.js to serve documentation from openapi.yaml.
    - Verified documentation accessibility at /api-docs.

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
 const bodyParser = require("body-parser");
 const lessonRoutesV1 = require("./routes/api/v1/lessons"); // Chemin relatif correct
 const homeRoute = require("./routes/webRoutes/home");
@@ -10,14 +12,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 // Routes
-app.use("/api/v1/lessons", lessonRoutesV1); 
+app.use("/api/v1/lessons", lessonRoutesV1);
 app.use("/", homeRoute);
+
+// charger le fichier OpenAPI
+const swaggerDocument = yaml.load("./docs/openapi.yaml");
+
+// configurer Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Démarrer le serveur
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  
+  console.log(`Swagger docs available on http://localhost:${PORT}/api-docs`);
 });
